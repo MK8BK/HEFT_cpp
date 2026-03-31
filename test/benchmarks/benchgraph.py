47ea1bbd05ff19ac27f45ee4d78c926eebb0b2b9
mk8bk <io.mk8bk@gmail.com>
2026-04-01 01:49:22 +0200

    added a 100k test

--- test/benchmarks/benchgraph.py
+++ test/benchmarks/benchgraph.py
@@ -3,14 +3,19 @@ import matplotlib.pyplot as plt
 #All graphs use max_dep 30
 #All graphs are scheduled on 20 processors
 
-task_numbers = [1000, 5000, 10000, 15000, 20000, 25000, 30000, 35000, 40000, 45000, 50000]
+task_numbers = [1000, 5000, 10000, 15000, 20000, 25000, 30000, 35000, 40000, 45000, 50000, 80000, 100000]
 
-scheduling_times = [0.023, 0.186, 0.447, 0.965, 1.790, 2.906, 4.127, 5.007, 6.217, 8.203, 10.532]
+
+
+
+
+
+scheduling_times = [0.023, 0.186, 0.447, 0.965, 1.790, 2.906, 4.127, 5.007, 6.217, 8.203, 10.532, 18.672,26.742]
 
 plt.plot(task_numbers, scheduling_times, "o-")
 plt.suptitle("20 processors  30 max_deps")
-plt.ylabel("Execution Time (s)")
-plt.xlabel("Number of tasks(s)")
+plt.ylabel("Execution Time")
+plt.xlabel("Number of Tasks")
 plt.show()
 
 
